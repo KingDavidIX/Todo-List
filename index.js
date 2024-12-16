723ff738950f341fd12ec8339a7684fc9ac15fd4
David Malkhasyan <malkhasyandavid2@gmail.com>
2024-12-16 11:55:27 +0400

    feat: Implement task management with Local Storage
    - Add functionality to create, mark as done, and delete tasks.
    - Persist tasks in Local Storage for retrieval on page reload.
    - Fix issues with immediate function invocation in event listeners.
    - Handle empty task input with an alert message.
    - Ensure tasks are correctly removed from Local Storage when deleted.
    - Optimize task rendering on page load to avoid duplicates.
    - Add state button to toggle task completion visually.

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ const input = document.getElementById('taskInput');
 const button = document.getElementById('addTaskBtn');
 const list = document.getElementById('taskList');
 
-button.addEventListener('click', () => {
-  const taskText = input.value.trim();
+let array = JSON.parse(localStorage.getItem('arr')) || [];
+function addTask(task) {
+  const taskText = task.trim();
 
   if (taskText !== '') {
+    if(!array.includes(task)){
+    array.push(taskText); 
+    localStorage.setItem('arr', JSON.stringify(array));
+    }
     const newTask = document.createElement('li');
     newTask.classList.add('task-item');
-    
+
     const taskContent = document.createElement('span');
     taskContent.textContent = taskText;
     newTask.appendChild(taskContent);
 
     const stateBtn = document.createElement('button');
+    stateBtn.textContent = 'V';
     stateBtn.classList.add('state-btn');
     stateBtn.addEventListener('click', () => {
        if (newTask.classList.contains('state-line')) {
@@ -28,17 +34,20 @@ button.addEventListener('click', () => {
     deleteBtn.classList.add('delete-btn');
     deleteBtn.addEventListener('click', () => {
       list.removeChild(newTask);
+      array = array.filter((item) => item !== taskText); 
+      localStorage.setItem('arr', JSON.stringify(array));
     });
 
-   
     newTask.appendChild(deleteBtn);
     newTask.appendChild(stateBtn);
-
-
     list.appendChild(newTask);
 
-    input.value = '';
+    input.value = ''; 
   } else {
     alert('Please enter a task!');
   }
-});
+}
+
+array.forEach((task) => addTask(task));
+
+button.addEventListener('click', () => addTask(input.value));
